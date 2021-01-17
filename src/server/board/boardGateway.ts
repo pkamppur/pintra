83512b103b22c765bc77897dc4724e196719e651
Petteri Kamppuri <petteri.kamppuri@me.com>
2021-01-17 10:36:23 +0200

    refactor: rename import

--- src/server/board/boardGateway.ts
+++ src/server/board/boardGateway.ts
@@ -1,4 +1,4 @@
-import { withDBClient } from 'server/db'
+import { withDBClient as _withDBClient } from 'server/db'
 import { Board, Id } from 'shared/board/model'
 
 const boards: Map<Id, Board> = new Map()
@@ -9,7 +9,7 @@ export async function fetchBoard(userId: Id, id: Id): Promise<Board> {
     return board
   }
 
-  const result = await withDBClient((client) => client.query('SELECT name, id FROM board WHERE id=$1', [id]))
+  const result = await _withDBClient((client) => client.query('SELECT name, id FROM board WHERE id=$1', [id]))
 
   if (result.rows.length == 0) {
     throw Error(`Board ${id} not found`)
