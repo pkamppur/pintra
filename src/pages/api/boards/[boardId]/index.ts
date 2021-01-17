a03478e41f8bdbb40a9f70c417c277e1bcd76f65
Petteri Kamppuri <petteri.kamppuri@me.com>
2021-01-17 19:38:24 +0200

    better import paths

--- src/pages/api/boards/[boardId]/index.ts
+++ src/pages/api/boards/[boardId]/index.ts
@@ -1,8 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { fetchBoard } from '../../../../server/board/boardGateway'
+import { fetchBoard } from 'server/board/boardGateway'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const boardId = req.query.boardId as string
+  console.log(`api boardId: ${boardId}`)
   const board = await fetchBoard('userId', boardId)
 
   res.status(200).json(board)
