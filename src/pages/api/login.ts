27a642305c16956bb5dd3ae8c50a89099f087f8e
Petteri Kamppuri <petteri.kamppuri@me.com>
2021-01-19 20:58:15 +0200

    /api/login for generating token for authenticated user

---
+++ src/pages/api/login.ts
@@ -0,0 +1,28 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { authenticate } from 'server/authentication'
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const body = req.body as { username?: string; password?: string }
+  const username = body.username
+  const password = body.password
+
+  if (username && password) {
+    const token = await authenticate(username, password)
+
+    if (!token) {
+      res.status(400)
+      res.setHeader('Content-Type', 'application/problem+json')
+      res.setHeader('Content-Language', 'en')
+      res.json({ title: 'Invalid username or password' })
+      return
+    }
+
+    res.status(200).json({ token })
+    return
+  }
+
+  res.status(400)
+  res.setHeader('Content-Type', 'application/problem+json')
+  res.setHeader('Content-Language', 'en')
+  res.json({ title: 'Missing parameters' })
+}
