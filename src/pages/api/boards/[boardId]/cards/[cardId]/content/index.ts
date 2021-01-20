1ef4e3ade3b1866b0eb53f62aebf6c0cf91f2648
Petteri Kamppuri <petteri.kamppuri@me.com>
2021-01-20 21:48:24 +0200

    authenticate api requests

--- src/pages/api/boards/[boardId]/cards/[cardId]/content/index.ts
+++ src/pages/api/boards/[boardId]/cards/[cardId]/content/index.ts
@@ -1,10 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { fetchCardContent } from 'server/board/boardGateway'
+import authenticatedRequest from 'server/authenticatedRequest'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const boardId = req.query.boardId as string
-  const cardId = req.query.cardId as string
-  const cardContent = await fetchCardContent(boardId, cardId)
+  authenticatedRequest(req, res, async (username) => {
+    const boardId = req.query.boardId as string
+    const cardId = req.query.cardId as string
+    const cardContent = await fetchCardContent(boardId, cardId)
 
-  res.status(200).json(cardContent)
+    res.status(200).json(cardContent)
+  })
 }
--- src/pages/api/boards/[boardId]/sections/index.ts
+++ src/pages/api/boards/[boardId]/sections/index.ts
@@ -1,9 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { fetchSections } from 'server/board/boardGateway'
+import authenticatedRequest from 'server/authenticatedRequest'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const boardId = req.query.boardId as string
-  const sections = await fetchSections(boardId)
+  authenticatedRequest(req, res, async (username) => {
+    const boardId = req.query.boardId as string
+    const sections = await fetchSections(boardId)
 
-  res.status(200).json(sections)
+    res.status(200).json(sections)
+  })
 }
