2886e574acd71ced6bfc2a8c4962868445330f37
Petteri Kamppuri <petteri.kamppuri@me.com>
2021-01-20 21:46:30 +0200

    better error reporting

--- src/components/board/useFetchBoard.ts
+++ src/components/board/useFetchBoard.ts
@@ -14,7 +14,32 @@ export function useFetchCardContent(boardId?: string, cardId?: string) {
 }
 
 export function useFetch<T>(urlOrPath: string | null) {
-  const { data, error } = useSWR<T>(urlOrPath)
+  const { data, error } = useSWR<T>(urlOrPath, fetcher)
+  console.log(`url ${urlOrPath}: data: ${JSON.stringify(data)}`)
 
   return { data, error, loading: !error && !data }
+}
+
+const fetcher = async (url: string) => {
+  const res = await fetch(url)
+  // If the status code is not in the range 200-299,
+  // we still try to parse and throw it.
+  if (!res.ok) {
+    const errorBody = await res.json()
+    const error = new HttpError('An error occurred while fetching the data.', res.status, errorBody)
+    // Attach extra info to the error object.
+    throw error
+  }
+  return res.json()
+}
+
+class HttpError extends Error {
+  status: number
+  bodyJson: any
+
+  constructor(message: string, status: number, bodyJson: any) {
+    super(message)
+    this.status = status
+    this.bodyJson = bodyJson
+  }
 }
