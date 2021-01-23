cbca46715f5fc933be7665256dd825c78f0092bc
Petteri Kamppuri <petteri.kamppuri@me.com>
2021-01-23 12:15:07 +0200

    refactor: extract methods etc.

--- src/server/board/boardGateway.ts
+++ src/server/board/boardGateway.ts
@@ -1,4 +1,4 @@
-import { PoolClient } from 'pg'
+import { Pool, PoolClient } from 'pg'
 import short from 'short-uuid'
 import { withDB as _withDB } from 'server/db'
 import { Board, Id, Section, Card, CardContent } from 'shared/board/model'
@@ -70,15 +70,20 @@ export async function fetchBoard(userId: Id, boardId: Id): Promise<Board> {
     return board
   }
 
-  const result = await withDB((db) => db.query('SELECT name, id FROM board WHERE id=$1', [boardId]))
+  const loadedBoard = await withDB((db) => boardFromDb(db, boardId))
 
-  if (result.rows.length == 0) {
+  boards.set(loadedBoard.id, loadedBoard)
+  return loadedBoard
+}
+
+async function boardFromDb(db: PoolClient, boardId: Id) {
+  const result = await db.query<Board>('SELECT name, id FROM board WHERE id=$1', [boardId])
+
+  if (result.rows.length === 0) {
     throw Error(`Board ${boardId} not found`)
-  } else {
-    const loadedBoard: Board = result.rows[0]
-    boards.set(loadedBoard.id, loadedBoard)
-    return loadedBoard
   }
+
+  return result.rows[0]
 }
 
 export async function addBoard(name: string): Promise<Board> {
@@ -102,22 +107,24 @@ interface DbSection {
 }
 
 export async function fetchSections(boardId: Id): Promise<Section[]> {
-  const sections = await withDB(async (db) => {
-    const dbSectionsRes = await db.query<DbSection>(
-      'SELECT name, id FROM board_section WHERE board_id=$1 ORDER BY position ASC',
-      [boardId]
-    )
+  const sections = await withDB(async (db) => sectionsForBoardFromDb(db, boardId))
 
-    const sections = await Promise.all(
-      dbSectionsRes.rows.map(async (dbSection) => {
-        const cards = await fetchCardsFromDb(db, dbSection.id)
+  return sections
+}
 
-        return { ...dbSection, cards }
-      })
-    )
+async function sectionsForBoardFromDb(db: PoolClient, boardId: Id): Promise<Section[]> {
+  const dbSectionsRes = await db.query<DbSection>(
+    'SELECT name, id FROM board_section WHERE board_id=$1 ORDER BY position ASC',
+    [boardId]
+  )
 
-    return sections
-  })
+  const sections = await Promise.all(
+    dbSectionsRes.rows.map(async (dbSection) => {
+      const cards = await fetchCardsFromDb(db, dbSection.id)
+
+      return { ...dbSection, cards }
+    })
+  )
 
   return sections
 }
