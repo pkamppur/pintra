91fa8b5d5174e583c9099161a5516b1311f8699a
Petteri Kamppuri <petteri.kamppuri@me.com>
2021-01-25 21:34:33 +0200

    refactor: rename file

--- src/server/board/boardContentGatewayProvider.ts
+++ src/server/board/boardContentGatewayProvider.ts
@@ -1,6 +1,6 @@
 import { Id } from 'shared/board/model'
 import { BoardContentGateway } from 'server/board/boardContentGateway'
-import { pgBoardContentGateway } from 'server/board/boardContentPGGateway'
+import { pgBoardContentGateway } from 'server/board/pgBoardContentGateway'
 
 export async function boardContentGateway(userId: Id, boardId: Id): Promise<BoardContentGateway> {
   return pgBoardContentGateway(userId, boardId)
