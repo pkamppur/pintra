82ccbb221d02c496b6e9d2ad3d67f7ac31bab937
Petteri Kamppuri <petteri.kamppuri@me.com>
2021-01-25 22:29:02 +0200

    error handling

--- src/pages/api/boards/[boardId]/content.ts
+++ src/pages/api/boards/[boardId]/content.ts
@@ -5,9 +5,16 @@ import authenticatedRequest from 'server/authenticatedRequest'
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   await authenticatedRequest(req, res, async (username) => {
     const boardId = req.query.boardId as string
-    const gateway = await boardContentGateway(username, boardId)
-    const board = await gateway.fetchBoardContent()
+    try {
+      const gateway = await boardContentGateway(username, boardId)
+      const board = await gateway.fetchBoardContent()
 
-    res.status(200).json(board)
+      res.status(200).json(board)
+    } catch (e) {
+      res.status(404)
+      res.setHeader('Content-Type', 'application/problem+json')
+      res.setHeader('Content-Language', 'en')
+      res.json({ title: 'Invalid request', status: 404 })
+    }
   })
 }
--- src/pages/api/boards/[boardId]/index.ts
+++ src/pages/api/boards/[boardId]/index.ts
@@ -6,8 +6,15 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   await authenticatedRequest(req, res, async (username) => {
     const boardId = req.query.boardId as string
 
-    const board = await fetchBoard('userId', boardId)
+    try {
+      const board = await fetchBoard('userId', boardId)
 
-    res.status(200).json(board)
+      res.status(200).json(board)
+    } catch (e) {
+      res.status(404)
+      res.setHeader('Content-Type', 'application/problem+json')
+      res.setHeader('Content-Language', 'en')
+      res.json({ title: 'Invalid request', status: 404 })
+    }
   })
 }
