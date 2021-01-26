f0b03b1cf0dc660d8348240d6650e16b13f8736b
Petteri Kamppuri <petteri.kamppuri@me.com>
2021-01-26 21:28:20 +0200

    error handling for route api/boards/[boardId]/cards/[cardId]/content/index.ts

--- src/pages/api/boards/[boardId]/cards/[cardId]/content/index.ts
+++ src/pages/api/boards/[boardId]/cards/[cardId]/content/index.ts
@@ -4,11 +4,19 @@ import { boardContentGateway } from 'server/board/boardGatewayProvider'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   await authenticatedRequest(req, res, async (username) => {
-    const boardId = req.query.boardId as string
-    const cardId = req.query.cardId as string
-    const gateway = await boardContentGateway(username, boardId)
-    const cardContent = await gateway.fetchCardContent(cardId)
+    try {
+      const boardId = req.query.boardId as string
+      const cardId = req.query.cardId as string
+      const gateway = await boardContentGateway(username, boardId)
+      const cardContent = await gateway.fetchCardContent(cardId)
 
-    res.status(200).json(cardContent)
+      res.status(200).json(cardContent)
+    } catch (e) {
+      console.log(`content error ${JSON.stringify(e)}, error ${e}`)
+      res.status(404)
+      res.setHeader('Content-Type', 'application/problem+json')
+      res.setHeader('Content-Language', 'en')
+      res.json({ title: 'Invalid request', status: 404 })
+    }
   })
 }
