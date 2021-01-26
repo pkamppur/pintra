e8cfecd40c4c4717dc7776776a927dd3ff5fd21e
Petteri Kamppuri <petteri.kamppuri@me.com>
2021-01-26 21:23:46 +0200

    remove unnecessary file

--- src/server/board/boardGateway.ts
+++
@@ -1,9 +0,0 @@
-import { PoolClient } from 'pg'
-import short from 'short-uuid'
-import { withDB as _withDB } from 'server/db'
-import { Board, Id } from 'shared/board/model'
-export async function fetchBoards(userId: Id): Promise<Board[]> {
-  // Disabled for privacy/security. One credential for now, so don't want to leak all boards to everyone sharing the same credentials.
-  // return await withDB((db) => boardsFromDb(db))
-  return []
-}
