36abb289b7a924028649ebeb2b2243300fe8a5a8
Petteri Kamppuri <petteri.kamppuri@me.com>
2021-01-26 21:23:23 +0200

    don't call non-existing code

--- src/pages/api/boards/index.ts
+++ src/pages/api/boards/index.ts
@@ -1,11 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { fetchBoards } from 'server/board/boardGateway'
 import authenticatedRequest from 'server/authenticatedRequest'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   await authenticatedRequest(req, res, async (username) => {
-    const boards = await fetchBoards(username)
-
-    res.status(200).json(boards)
+    // Unimplemented for now because of privacy/security
+    res.status(200).json([])
   })
 }
