028eacdef649c73f04822701ee22ce8b0824e40c
Petteri Kamppuri <petteri.kamppuri@me.com>
2021-01-30 13:31:51 +0200

    log search api endpoint errors to console, return error status

--- src/pages/api/boards/[boardId]/search.ts
+++ src/pages/api/boards/[boardId]/search.ts
@@ -7,13 +7,21 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     const boardId = req.query.boardId as string
     const query = req.query.query as string
 
-    if (!username || !boardId || !query) {
-      console.log(`searchResults No content found`)
-      res.status(404).json({ title: 'No content found' })
-    }
-    const gateway = await boardContentGateway(username, boardId)
-    const searchResults = await gateway.searchCards(query)
+    try {
+      if (!username || !boardId || !query) {
+        console.log(`searchResults No content found`)
+        res.status(404).json({ title: 'No content found' })
+      }
+      const gateway = await boardContentGateway(username, boardId)
+      const searchResults = await gateway.searchCards(query)
 
-    res.status(200).json(searchResults)
+      res.status(200).json(searchResults)
+    } catch (e) {
+      console.log(`[search] error ${JSON.stringify(e)}, error ${e}`)
+      res.status(404)
+      res.setHeader('Content-Type', 'application/problem+json')
+      res.setHeader('Content-Language', 'en')
+      res.json({ title: 'Invalid request', status: 404 })
+    }
   })
 }
