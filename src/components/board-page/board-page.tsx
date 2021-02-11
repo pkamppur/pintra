126542e834daefa1138e21ca0d10a41375e9ce2d
Petteri Kamppuri <petteri.kamppuri@me.com>
2021-02-11 19:59:40 +0200

    refactor: extract board page

---
+++ src/components/board-page/board-page.tsx
@@ -0,0 +1,35 @@
+import { CSSProperties } from 'react'
+import { BoardContent, BoardStyles } from 'shared/board/model'
+import BoardContents from 'components/board/boardContents'
+import styles from './board-page.module.scss'
+
+interface BoardPageProps {
+  board: BoardContent
+}
+
+export default function BoardPage({ board }: BoardPageProps) {
+  return (
+    <main className={styles.main} style={stylesForBoardStyles(board.styles)}>
+      <BoardContents board={board} />
+    </main>
+  )
+}
+
+function stylesForBoardStyles(styles: BoardStyles): CSSProperties {
+  if (styles.backgroundImage) {
+    return {
+      backgroundImage: styles.backgroundImage,
+      backgroundSize: 'cover',
+    }
+  } else if (styles.background) {
+    return {
+      background: styles.background,
+    }
+  } else if (styles.backgroundColor) {
+    return {
+      backgroundColor: styles.backgroundColor,
+    }
+  } else {
+    return {}
+  }
+}
--- src/pages/b/[boardId].tsx
+++ src/pages/b/[boardId].tsx
@@ -1,11 +1,11 @@
 import { useRouter } from 'next/router'
-import { CSSProperties, Dispatch, SetStateAction, useState } from 'react'
-import { BoardContent, BoardStyles } from 'shared/board/model'
+import { Dispatch, SetStateAction, useState } from 'react'
+import { BoardContent } from 'shared/board/model'
 import Scaffold from 'components/scaffold'
 import { useFetchBoardContent, useFetchSearch } from 'components/board/useFetchBoard'
 import { asString } from 'components/stringHelpers'
 import SearchBox from 'components/searchbox/searchbox'
-import BoardContents from 'components/board/boardContents'
+import BoardPage from 'components/board-page/board-page'
 import styles from './board.module.scss'
 
 export default function DynamicBoardPage() {
@@ -55,28 +55,7 @@ function LoadedBoard({ board, setSearchTerm, pagePath }: BoardPageProps) {
 
   return (
     <Scaffold title={`${board.name} | Pintra`} loginRedirect={pagePath} additionalNavComponent={navBarItems}>
-      <main className={styles.main} style={stylesForBoardStyles(board.styles)}>
-        <BoardContents board={board} />
-      </main>
+      <BoardPage board={board} />
     </Scaffold>
   )
-}
-
-function stylesForBoardStyles(styles: BoardStyles): CSSProperties {
-  if (styles.backgroundImage) {
-    return {
-      backgroundImage: styles.backgroundImage,
-      backgroundSize: 'cover',
-    }
-  } else if (styles.background) {
-    return {
-      background: styles.background,
-    }
-  } else if (styles.backgroundColor) {
-    return {
-      backgroundColor: styles.backgroundColor,
-    }
-  } else {
-    return {}
-  }
 }
